b784260cedf1bcd032f232d40b0efd8ee7d995a3
Ryan Salim <ryansalimm@gmail.com>
2018-12-12 14:25:38 +0530

    Initial Commit

---
+++ TestAppStoreAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppStoreAnimation
+//
+//  Created by Ryan Salim on 12/12/18.
+//  Copyright © 2018 Ryan Salim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
